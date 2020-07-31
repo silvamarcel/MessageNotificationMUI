2aa9c8c4ceaf4b84a561e527717a1ff429596271
Marcel Silva <silva.marcel@gmail.com>
2020-07-31 19:54:26 +0000

    Updates

---
+++ precache-manifest.995917d0561ce12a7963d265f6bb5c69.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "153b204d56f3f6f1e2d27f9ffcbd7b6e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "50873e0d1d0bcf440ff3",
+    "url": "./static/js/2.b6b19506.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "./static/js/2.b6b19506.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1880969ed71d25cb2fd",
+    "url": "./static/js/main.fece2183.chunk.js"
+  },
+  {
+    "revision": "afb86acfae5bcf963b69",
+    "url": "./static/js/runtime-main.0a21a14a.js"
+  }
+]);
